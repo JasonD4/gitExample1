8241080dcfda94c0b163b6c77cc56ef59cb94393
Jason <Jason@Pursuits-MacBook-Pro-42.local>
2019-02-08 10:55:05 -0500

    first Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Jason on 2/8/19.
+//  Copyright © 2019 Jason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Working on git")
+    }
+
+
+}
+
